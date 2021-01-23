097b8e05f728566910e8a15844f048f7e843903d
Aditya Ramakrishnan <adityarules2013@gmail.com>
2021-01-22 17:14:12 -0800

    Update DisplayMapActivity.kt

--- app/src/main/java/com/example/mymaps/DisplayMapActivity.kt
+++ app/src/main/java/com/example/mymaps/DisplayMapActivity.kt
@@ -24,21 +24,11 @@ class DisplayMapActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_display_map)
 
         userMap = intent.getSerializableExtra(EXTRA_USER_MAP) as UserMap
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -51,7 +41,6 @@ class DisplayMapActivity : AppCompatActivity(), OnMapReadyCallback {
             boundsBuilder.include(latLng)
             mMap.addMarker(MarkerOptions().position(latLng).title(place.title).snippet(place.description))
         }
-        // Add a marker in Sydney and move the camera
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 1000, 1000, 0))
     }
 }
